b6bcf926ffccde563159332714af00e406ee5565
jallen2112 <jallen65@mail.csuchico.edu>
2020-05-04 12:37:25 -0700

    sample tests that output errors to slack_output.md file

--- validate/lab01_test.go
+++ validate/lab01_test.go
@@ -2,6 +2,7 @@ package validate
 
 import (
 	"io/ioutil"
+	"log"
 
 	"gopkg.in/yaml.v2"
 
@@ -9,18 +10,24 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-
 var _ = Describe("Lab 1 Containers", func() {
+	var failMessage string
+
+	BeforeEach(func() {
+		failMessage = ""
+	})
+
 	Context("Step 2", func() {
 		It("should have a Dockerfile", func() {
-			Expect(fileExists("../Dockerfile")).To(Succeed(), "Dockerfile was not found in the springtrader folder. Maybe the file was not saved or it was saved to a different folder.")
+			failMessage = "Dockerfile Doesn't Exist or is in the wrong location\n"
+			Expect(fileExists("../Dockerfile")).To(Succeed(), failMessage)
 		})
 	})
 
 	Context("Step 3", func() {
 		It("should have a skaffold.yaml file", func() {
-			Expect(fileExists("../skaffold.yaml")).To(Succeed(), "skaffold.yaml was not found in the springtrader folder. Maybe the file was not saved or it was save in a different folder.")
-			Fail("foo")
+			failMessage = "skaffold.yaml Doesn't Exist or is in the wrong location\n"
+			Expect(fileExists("../skaffold.yaml")).To(Succeed(), failMessage)
 		})
 
 		It("should be a valid skaffold configuration", func() {
@@ -29,11 +36,26 @@ var _ = Describe("Lab 1 Containers", func() {
 			if err != nil {
 				Skip("skaffold.yaml not found")
 			}
+
 			err = yaml.Unmarshal(skaffoldFile, &skaffold)
 			Expect(err).ToNot(HaveOccurred())
-			Expect(treeValue(skaffold, []interface{}{"apiVersion"})).To(Equal("skaffold/v1beta12"))
-			Expect(treeValue(skaffold, []interface{}{"build", "artifacts", 0, "image"})).To(Equal("springtrader"))
-			Expect(treeValue(skaffold, []interface{}{"build", "artifacts", 1, "image"})).To(Equal("sqlfdb"))
+			//failures := InterceptGomegaFailures(func() {
+
+			failMessage = "Incorrect apiVersion in skaffold.yaml\n"
+			Expect(treeValue(skaffold, []interface{}{"apiVersion"})).To(Equal("skaffold/v1beta12"), failMessage)
+			failMessage = "First build artifact in skaffold.yaml should be \"springtrader\"\n"
+			Expect(treeValue(skaffold, []interface{}{"build", "artifacts", 0, "image"})).To(Equal("springtrader"), failMessage)
+			failMessage = "Second build artifact in skaffold.yaml should be \"sqlfdb\"\n"
+			Expect(treeValue(skaffold, []interface{}{"build", "artifacts", 1, "image"})).To(Equal("sqlfdb"), failMessage)
+			//})
+			//log.Printf(failures[0])
 		})
+	})
+
+	AfterEach(func() {
+		log.Printf("%v\n", CurrentGinkgoTestDescription())
+		if CurrentGinkgoTestDescription().Failed {
+			ConcatenatedMessage += failMessage
+		}
 	})
 })
